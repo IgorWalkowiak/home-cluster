756f293d51483a842fa8481304de0241b2045423
Igor <igorwalkowiak@protonmail.com>
2024-12-08 21:25:08 +0100

    Another thest

--- init/init.sh
+++ init/init.sh
@@ -4,6 +4,7 @@ RELEASE_NAME="argo"
 NAMESPACE="admin"
 ARGO_VALUES="argo-cd/values.yaml"
 REPO_FILE="argo-cd/repo.yaml"
+APP_OF_APPS_FILE="argo-cd/app-of-apps.yaml"
 
 kubectl create namespace $NAMESPACE
 
@@ -21,8 +22,11 @@ else
   echo "Admin password: $PASSWORD"
 fi
 
-echo "Applying repo.yaml to add the repository..."
+echo "Applying $REPO_FILE to add the repository..."
 kubectl apply -f $REPO_FILE -n $NAMESPACE
+
+echo "Applying $APP_OF_APPS_FILE to add the repository..."
+kubectl apply -f $APP_OF_APPS_FILE -n $NAMESPACE
 
 echo "Setting up port forwarding (localhost:8080 -> service:80)..."
 kubectl port-forward svc/${RELEASE_NAME}-argo-cd-server 8080:80 -n $NAMESPACE &
